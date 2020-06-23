a4163c90dcc0520d6817effd4fa6f0edc313c537
Md Sahil <287mdsahil@gmail.com>
2020-06-24 00:07:59 +0530

    Added background video

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
 
+import introVideo from "./res/video/intro.mp4";
+
 function App() {
   return (
-    <div
-      className="App"
-    >
+    <div className="App">
+      <video
+        autoPlay
+        loop
+        muted
+        style={{
+          maxWidth: "100%",
+          maxHeight: "100%",
+          objectFit: "cover",
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)"
+        }}
+      >
+        <source src={introVideo} type="video/mp4" />
+      </video>
       test
     </div>
   );
