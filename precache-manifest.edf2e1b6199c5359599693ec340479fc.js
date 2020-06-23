71c0eb2c3235487f0ed9cdd9beace6334f1dd0dd
Md Sahil <287mdsahil@gmail.com>
2020-06-24 00:51:48 +0530

    Updates

---
+++ precache-manifest.edf2e1b6199c5359599693ec340479fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adaee4402a3eee13c1a9f8bcbc3cd797",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f9f4ced3c087872ee50",
+    "url": "/static/js/2.362a31d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.362a31d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4da12a5be4ad6b3bd340",
+    "url": "/static/js/main.f3a8c6bf.chunk.js"
+  },
+  {
+    "revision": "84036689668aced7746d",
+    "url": "/static/js/runtime-main.1ba42e3a.js"
+  },
+  {
+    "revision": "e0970c9a425321894d42fdb22bb8697d",
+    "url": "/static/media/intro.e0970c9a.mp4"
+  }
+]);
