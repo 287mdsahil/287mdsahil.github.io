f6e71c87ad1b0e96939cd7750505264a46dc6086
Md Sahil <287mdsahil@gmail.com>
2020-06-28 22:09:17 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 import introVideo from "./res/media/videos/FourierOfS/1440p60/FourierOfS.mp4";
+import profileImg from "./img/profile_img_bw.png";
 
 function App() {
   return (
-    <div className="App">
-      <video
-        autoPlay
-        loop
-        muted
-        style={{
-          maxWidth: "100%",
-          maxHeight: "100%",
-          objectFit: "cover",
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)"
-        }}
-      >
-        <source src={introVideo} type="video/mp4" />
-      </video>
-      test
-    </div>
+    <Container className="App">
+      <Row>
+        <center>
+          <video
+            autoPlay
+            loop
+            muted
+            style={{
+              maxWidth: "100vw",
+              maxHeight: "90vh"
+            }}
+          >
+            <source src={introVideo} type="video/mp4" />
+          </video>
+        </center>
+      </Row>
+      <Row>
+        <center>
+          <img
+            src={profileImg}
+            alt=""
+            style={{
+              maxWidth: "70vw",
+              maxHeight: "90vh",
+              margin: "auto"
+            }}
+          />
+        </center>
+      </Row>
+    </Container>
   );
 }
 
