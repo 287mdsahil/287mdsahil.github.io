92c1689a60323851d130b6bb94515d9ce04be7ee
Md Sahil <287mdsahil@gmail.com>
2020-07-08 01:18:46 +0530

    Updates

---
+++ precache-manifest.4710341521ef7618526010d31390a371.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf0676745a007f75ba819f2084b70dd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abf060916cac2c012eb2",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "9201e638800a3e70993e",
+    "url": "/static/css/main.cbddc483.chunk.css"
+  },
+  {
+    "revision": "abf060916cac2c012eb2",
+    "url": "/static/js/2.32425c41.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.32425c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9201e638800a3e70993e",
+    "url": "/static/js/main.172d3fbe.chunk.js"
+  },
+  {
+    "revision": "84036689668aced7746d",
+    "url": "/static/js/runtime-main.1ba42e3a.js"
+  },
+  {
+    "revision": "8b71b66b9b82d29e6b8815fce3153d0a",
+    "url": "/static/media/profile_img.8b71b66b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a513368070dd3dc2c87b6a439f51305e.js"
+  "/precache-manifest.4710341521ef7618526010d31390a371.js"
 );
 
 self.addEventListener('message', (event) => {
